0104973cfbd2c2e54f5e8bffe572fc202c773a53
Yeghishe <marsel8937@gmail.com>
2023-03-15 10:30:42 +0000

    creating public subnet outputs

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,12 @@ output "db-subnet-B" {
   value = aws_subnet.db-sub-B.id
 }
 
+output "pub-subnet-A" {
+  value = aws_subnet.pub-sub-A.id
+}
+
+
+output "pub-subnet-B" {
+  value = aws_subnet.pub-sub-B.id
+}
+
