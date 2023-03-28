527e398592294394b6c9402e7bc34b810004bff6
Yeghishe <marsel8937@gmail.com>
2023-03-28 15:09:22 +0000

    changing gateway_id to nat_gateway_id

--- network.tf
+++ network.tf
@@ -118,7 +118,7 @@ resource "aws_route_table" "priv-route" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat-gw.id
+    nat_gateway_id = aws_nat_gateway.nat-gw.id
   }
   tags = {
     Name = "Private Route"
